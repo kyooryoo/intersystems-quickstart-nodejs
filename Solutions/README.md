a22cf6e256544c0087563b48b9dc2c0edbd41a82
Ling Jiang <kyooryoo@hotmail.com>
2022-02-21 12:53:42 +0000

    second commit

---
+++ Solutions/README.md
@@ -0,0 +1,3 @@
+# Nodejs sample code from InterSystems
+For demo of the Native API of InterSystems' IRIS.  
+Log in management console for connection details.  
--- Solutions/nodeplaystocksTasks.js
+++ Solutions/nodeplaystocksTasks.js
@@ -1,20 +1,25 @@
 /*
-* PURPOSE: Store stock data directly to InterSystems IRIS Data Platform using a custom structure.
+* PURPOSE: Store stock data directly to InterSystems IRIS Data Platform using a custom structure and generate trade data
+*   with methods from InterSystems IRIS as well as call routine to print the version of InterSystems IRIS.
 * 
-* NOTES: When running, choose option 2 to store stock data natively.
+* NOTES: When running,
+* 1. Choose option 2 to store stock data natively.
+* 2. Choose option 3 to retrieve stock data natively.
+* 3. Choose option 4 to generate trades with random data using methods from InterSystems IRIS.
+* 4. Choose option 5 to call InterSystems IRIS routine directly.
 */
 
 const irisnative = require('intersystems-iris-native')
 const readline = require('readline-sync');
-const fs = require("fs");
+const fs = require('fs');
 
 // Helper method: Get connection details from config file
 function GetConnections(filename){
     var connections = {};
     var array = fs.readFileSync(filename).toString().split("\n");
     
+    // Remove all spaces and split line based on colon
     for(i in array) {
-        // Remove all spaces and split line based on colon
         var details = array[i].replace(/\s/g, '').split(":")
         connections[details[0]] = details[1]
     }
@@ -23,7 +28,7 @@ function GetConnections(filename){
 
 function main()
 {
-    // Get connection details from connections.config 
+    // Get connection details from connections.config
     var connections = GetConnections("connections.config");
 
     // Retrieve connection information from configuration file
@@ -44,7 +49,7 @@ function main()
     while(true)
     {
         console.log("1. Test");
-        console.log("2. Store stock data");
+        console.log("2. Store Stock Data");
         console.log("3. View stock data");
         console.log("4. Generate Trades");
         console.log("5. Call routines");
@@ -58,13 +63,13 @@ function main()
                 StoreStockData(iris);
                 break;
             case "3":
-                console.log("TO DO: View stock data");
+                ViewStockData(iris);
                 break;
             case "4":
-                console.log("TO DO: Generate trades");
+                PopulateData(iris, 10);
                 break;
             case "5":
-                console.log("TO DO: Call routines");
+                callRoutines(iris);
                 break;
             case "6":
                 console.log("Exited");
@@ -73,25 +78,28 @@ function main()
                 console.log("Invalid option. Try again!");
                 break;
         }
-    }    
+
+    }
 }
 
 // Write to a test global
-function SetTestGlobal(irisNative){
-    irisNative.set(8888, "^testglobal", "1");
-    globalValue = irisNative.get("^testglobal", "1");
-    console.log("The value of ^testglobal(1) is " + globalValue);
-}
+function SetTestGlobal(irisNative)
+	{
+		irisNative.set(8888, "^testglobal", "1");
+		globalValue = irisNative.get("^testglobal", "1");
+		console.log("The value of ^testglobal(1) is " + globalValue);
+    }
 
 // Store stock data directly into InterSystems IRIS
 function StoreStockData(irisNative){
     // Clear global from previous runs
     irisNative.kill("^nyse");
-
+    
     console.log("Storing stock data using Native API...");
 
     // Get all stock data from all_stocks.csv file into a list
     var array = fs.readFileSync('all_stocks.csv').toString().split("\n");
+    console.log("One sample stock data is as follows:\n" + array[1]);
 
     // Get start time
     var start = Date.now()/1000;
@@ -107,5 +115,39 @@ function StoreStockData(irisNative){
     console.log("Stored natively successfully. Execution time: " + totalConsume + "ms");
 }
 
+// Iterate over all nodes and print
+function ViewStockData(irisNative)
+	{
+        // Create iterator
+		var iter = irisNative.iterator("^nyse");
+        console.log("Walk forwards");
+
+        // Iterate over all nodes
+        for ([key, value] of iter){
+            console.log("subscript = " + key + ", value = " + value);
+        }
+
+    }
+
+// To generate the list of trades with methods from InterSystems IRIS    
+function PopulateData(irisNative, object_count)
+	{
+        // Loop through list of trade to generate data for each trade
+		for(var i = 0; i < object_count; i++){
+            tempDate = "2020-01-01";
+            tempAmount = irisNative.classMethodValue("%PopulateUtils", "Currency");
+            tempName = 	irisNative.classMethodValue("%PopulateUtils", "String") + 
+                        irisNative.classMethodValue("%PopulateUtils", "String") + 
+                        irisNative.classMethodValue("%PopulateUtils", "String");
+            tempTrader = irisNative.classMethodValue("%PopulateUtils", "Name");
+            tempShares = Math.floor(Math.random() * 10);
+            console.log("New trade: " + tempName + ", " + tempDate + ", " + tempAmoun5t + ", " + tempShares + ", " + tempTrader);
+        }
+    }
+
+// Call routine directly to print the version of InterSystems IRIS.    
+function callRoutines(irisNative){
+    console.log("on InterSystems IRIS version:\n" + irisNative.function("StocksUtil", "PrintVersion"))
+}
     
 main()
